229d6b3c4ef4a67027bb467a36f45294f410efa3
Brian Griggs <bgriggs@econolite.com>
2020-10-25 15:00:46 -0600

    Updating status consumers to use latest status. #148

--- aspnet-core/common/BigMission.CommandTools/AppCommands.cs
+++ aspnet-core/common/BigMission.CommandTools/AppCommands.cs
@@ -1,14 +1,11 @@
 ﻿using Azure.Messaging.EventHubs;
 using Azure.Messaging.EventHubs.Consumer;
-using Azure.Messaging.EventHubs.Processor;
 using Azure.Messaging.EventHubs.Producer;
-using Azure.Storage.Blobs;
 using BigMission.CommandTools.Models;
 using Newtonsoft.Json;
 using NLog;
 using System;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace BigMission.CommandTools
@@ -21,85 +18,57 @@ namespace BigMission.CommandTools
         const string DESTID = "DestId";
         private ILogger Logger { get; }
 
-        private EventProcessorClient processor;
         private Action<Command> commandCallback;
+        private readonly EventHubHelpers ehReader;
+
         private bool disposed;
 
         private readonly string appId;
         private readonly string kafkaConnStr;
         private readonly string groupId;
         private readonly string topic;
-        private readonly string blobStorageConnStr;
-        private readonly string blobContainer;
 
-        public AppCommands(string appId, string kafkaConnStr, string groupId, string topic, string blobStorageConnStr, string blobContainer, ILogger logger)
+        public AppCommands(string appId, string kafkaConnStr, string groupId, string topic, ILogger logger)
         {
             this.appId = appId;
             this.kafkaConnStr = kafkaConnStr;
             this.groupId = groupId;
             this.topic = topic;
-            this.blobStorageConnStr = blobStorageConnStr;
-            this.blobContainer = blobContainer;
             Logger = logger;
+            ehReader = new EventHubHelpers(logger);
         }
 
-        public void ListenForCommands(Action<Command> commandCallback)
+        public async Task ListenForCommandsAsync(Action<Command> commandCallback)
         {
             this.commandCallback = commandCallback ?? throw new InvalidOperationException();
 
-            // Process changes from stream and cache them here is the service
-            var storageClient = new BlobContainerClient(blobStorageConnStr, blobContainer);
-            processor = new EventProcessorClient(storageClient, groupId, kafkaConnStr, topic);
-            processor.ProcessEventAsync += CommandProcessEventHandler;
-            processor.ProcessErrorAsync += CommandProcessErrorHandler;
-            processor.PartitionInitializingAsync += Processor_PartitionInitializingAsync;
-            processor.StartProcessing();
+            await ehReader.ReadEventHubPartitionsAsync(kafkaConnStr, topic, groupId, null, EventPosition.Latest, ReceivedEventCallback);
         }
 
-        private Task Processor_PartitionInitializingAsync(PartitionInitializingEventArgs arg)
-        {
-            arg.DefaultStartingPosition = EventPosition.Latest;
-            return Task.CompletedTask;
-        }
-
-        private async Task CommandProcessEventHandler(ProcessEventArgs eventArgs)
-        {
-            if (eventArgs.Data.Properties.TryGetValue(DESTID, out object destObj))
-            {
-                if (destObj.ToString() == appId)
-                {
-                    var json = Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray());
-                    var cmd = JsonConvert.DeserializeObject<Command>(json);
-                    if (cmd != null)
-                    {
-                        Logger.Info($"Received command: '{cmd.CommandType}'");
-                        ThreadPool.QueueUserWorkItem(ProcessCallback, cmd);
-                    }
-                }
-            }
-            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
-            await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
-        }
-
-        private Task CommandProcessErrorHandler(ProcessErrorEventArgs eventArgs)
-        {
-            // Write details about the error to the console window
-            Logger.Error(eventArgs.Exception, $"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered.");
-            return Task.CompletedTask;
-        }
-
-        private void ProcessCallback(object o)
+        private void ReceivedEventCallback(PartitionEvent receivedEvent)
         {
             try
             {
-                var cmd = (Command)o;
-                commandCallback(cmd);
+                if (receivedEvent.Data.Properties.TryGetValue(DESTID, out object destObj))
+                {
+                    if (destObj.ToString() == appId)
+                    {
+                        Logger.Trace($"Received command on partition {receivedEvent.Partition.PartitionId}");
+                        var json = Encoding.UTF8.GetString(receivedEvent.Data.Body.ToArray());
+                        var cmd = JsonConvert.DeserializeObject<Command>(json);
+                        if (cmd != null)
+                        {
+                            commandCallback(cmd);
+                        }
+                    }
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                Logger.Error(ex, "Unable to complete command callback.");
+                Logger.Error(ex, "Unable to process event from event hub partition");
             }
         }
+
 
         public async Task SendCommand(Command command)
         {
@@ -147,7 +116,7 @@ namespace BigMission.CommandTools
 
             if (disposing)
             {
-               
+
             }
 
             disposed = true;
---
+++ aspnet-core/common/BigMission.CommandTools/EventHubHelpers.cs
@@ -0,0 +1,85 @@
+﻿using Azure.Messaging.EventHubs.Consumer;
+using Newtonsoft.Json;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BigMission.CommandTools
+{
+    public class EventHubHelpers
+    {
+        private Action<PartitionEvent> callback;
+        private ILogger Logger { get; }
+        private readonly CancellationTokenSource cancellationToken = new CancellationTokenSource();
+
+
+        public EventHubHelpers(ILogger logger)
+        {
+            Logger = logger ?? throw new InvalidOperationException();
+        }
+
+
+        public async Task ReadEventHubPartitionsAsync(string ehConnection, string ehName, string consumerGroup, 
+            string[] partitionIdsFilter, EventPosition eventPosition,  Action<PartitionEvent> callback)
+        {
+            this.callback = callback ?? throw new InvalidOperationException();
+
+            await using var consumer = new EventHubConsumerClient(consumerGroup, ehConnection, ehName);
+
+            var activePartitions = await consumer.GetPartitionIdsAsync();
+
+            var partitionConsumers = new List<Task>();
+            foreach (var partitionId in activePartitions)
+            {
+                if (partitionIdsFilter == null || partitionIdsFilter.Contains(partitionId))
+                {
+                    Task read = ReadPartition(consumer, partitionId, eventPosition);
+                    partitionConsumers.Add(read);
+                }
+                else
+                {
+                    Logger.Warn($"Partition {partitionId} does not exist, skipping");
+                }
+            }
+
+            Task.WaitAll(partitionConsumers.ToArray());
+        }
+
+        private async Task ReadPartition(EventHubConsumerClient consumer, string partitionId, EventPosition eventPosition)
+        {
+            await foreach (PartitionEvent receivedEvent in consumer.ReadEventsFromPartitionAsync(partitionId, eventPosition, cancellationToken.Token))
+            {
+                try
+                {
+                    ThreadPool.QueueUserWorkItem(ProcessCallback, receivedEvent);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Unable to process event from event hub partition");
+                }
+            }
+        }
+
+        private void ProcessCallback(object o)
+        {
+            try
+            {
+                var receivedEvent = (PartitionEvent)o;
+                callback(receivedEvent);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Unable to complete event callback.");
+            }
+        }
+
+        public void CancelProcessing()
+        {
+            cancellationToken.Cancel();
+        }
+    }
+}
