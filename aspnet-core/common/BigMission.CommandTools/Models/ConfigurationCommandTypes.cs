2a0f9f85e8a06cd275b811cf64c043035df2ccb0
Brian Griggs <bgriggs@econolite.com>
2021-03-23 20:02:42 -0600

    Config in validate. Fixes #255. Fixes #254

--- aspnet-core/common/BigMission.CommandTools/Models/ConfigurationCommandTypes.cs
+++ aspnet-core/common/BigMission.CommandTools/Models/ConfigurationCommandTypes.cs
@@ -6,5 +6,6 @@
         public const string CHANNEL_MODIFIED = "ChannelModified";
         public const string EVENT_SUBSCRIPTION_MODIFIED = "EventSubscriptionModified";
         public const string ACTIVE_DRIVER = "ActiveDriver";
+        public const string ALARM_CHANGED = "AlarmChanged";
     }
 }
