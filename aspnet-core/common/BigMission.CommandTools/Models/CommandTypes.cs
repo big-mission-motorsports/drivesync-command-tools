b5c51364da26a1954241fefcc2b0d3704720e801
Brian Griggs <brian@econolite.com>
2021-05-26 18:06:18 -0600

    Adding keypad led status and command. Fixes # 291

--- aspnet-core/common/BigMission.CommandTools/Models/CommandTypes.cs
+++ aspnet-core/common/BigMission.CommandTools/Models/CommandTypes.cs
@@ -5,5 +5,6 @@
         public const string UPDATE_CONFIG = "UpdateConfig";
         public const string SEND_CAN = "SendCan";
         public const string SET_LOG_LEVEL = "SetLogLevel";
+        public const string SEND_KEYPAD_BUTTON_PRESS = "SendKeypadButtonPress";
     }
 }
