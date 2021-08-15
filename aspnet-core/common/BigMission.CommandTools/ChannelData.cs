64d4eaa5e5a4d585cd0bf4c7a8d0b80278de32f5
Brian Griggs <brian@bigmissionmotorsports.com>
2021-08-15 15:48:07 -0600

    Adding interface for race hero source.

--- aspnet-core/common/BigMission.CommandTools/ChannelData.cs
+++ aspnet-core/common/BigMission.CommandTools/ChannelData.cs
@@ -2,6 +2,7 @@
 using Azure.Messaging.EventHubs.Producer;
 using BigMission.DeviceApp.Shared;
 using Newtonsoft.Json;
+using System;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,29 +11,43 @@ namespace BigMission.CommandTools
     /// <summary>
     /// Sends CAN Bus cannel data.
     /// </summary>
-    public class ChannelData
+    public class ChannelData : IAsyncDisposable
     {
         private readonly string appId;
         private readonly string kafkaConnStr;
         private readonly string topic;
+        private readonly EventHubProducerClient producerClient;
 
         public ChannelData(string appId, string kafkaConnStr, string topic)
         {
             this.appId = appId;
             this.kafkaConnStr = kafkaConnStr;
             this.topic = topic;
+            producerClient = new EventHubProducerClient(kafkaConnStr, topic);
         }
 
 
         public async Task SendData(ChannelDataSetDto dataSet)
         {
-            await using var producerClient = new EventHubProducerClient(kafkaConnStr, topic);
             using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
             var data = JsonConvert.SerializeObject(dataSet);
             var eventData = new EventData(Encoding.UTF8.GetBytes(data));
             eventData.Properties["DeviceAppId"] = appId;
             eventBatch.TryAdd(eventData);
             await producerClient.SendAsync(eventBatch);
+        }
+
+
+        public virtual ValueTask DisposeAsync()
+        {
+            try
+            {
+                return producerClient.DisposeAsync();
+            }
+            catch (Exception exception)
+            {
+                return new ValueTask(Task.FromException(exception));
+            }
         }
     }
 }
