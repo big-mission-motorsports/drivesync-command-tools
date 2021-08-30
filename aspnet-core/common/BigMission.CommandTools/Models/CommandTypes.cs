d4cfbd0331ab7910085b2600e1e23ef7bd63550a
Brian Griggs <brian@bigmissionmotorsports.com>
2021-08-30 16:21:29 -0600

    Adding restart command to device app. #347

--- aspnet-core/common/BigMission.CommandTools/Models/CommandTypes.cs
+++ aspnet-core/common/BigMission.CommandTools/Models/CommandTypes.cs
@@ -6,5 +6,6 @@
         public const string SEND_CAN = "SendCan";
         public const string SET_LOG_LEVEL = "SetLogLevel";
         public const string SEND_KEYPAD_BUTTON_PRESS = "SendKeypadButtonPress";
+        public const string RESTART = "Restart";
     }
 }
